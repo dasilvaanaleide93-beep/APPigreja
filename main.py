1bc49916a2a959d8483cdaf0bca1aa4e49172c95
ana leide cardoso da silva ferreira <dasilvaanaleide93@gmail.com>
2026-09-17 23:28:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ CORS(app)
 
 ARQUIVO = "pedidos.json"
 
-# carrega o que já tava salvo
 if os.path.exists(ARQUIVO):
     with open(ARQUIVO, "r", encoding="utf-8") as f:
         pedidos_oracao = json.load(f)
@@ -19,17 +18,20 @@ def salvar():
     with open(ARQUIVO, "w", encoding="utf-8") as f:
         json.dump(pedidos_oracao, f, ensure_ascii=False, indent=2)
 
-@app.route('/oracoes', methods=['GET', 'POST'])
-def oracoes():
+@app.route('/', methods=['GET'])
+def inicio():
+    return "App Igreja no ar! Deus abencoe 🙏 Acesse /oracao"
+
+@app.route('/oracao', methods=['GET', 'POST'])
+def oracao():
     if request.method == 'POST':
         dados = request.get_json()
         pedidos_oracao.append(dados)
-        salvar() # <- ESSA LINHA SALVA!
-        print(f"novo pedido: {dados} -> SALVO!")
+        salvar()
+        print(f"novo pedido: {dados}")
         return jsonify({"mensagem": "Recebido!"})
     return jsonify({"oracoes": pedidos_oracao})
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
-
